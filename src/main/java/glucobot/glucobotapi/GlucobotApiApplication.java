f50b04c31a778ab95d5ee0cf2ad665fefe42c6dd
Carl Rizk <carlrizk88@gmail.com>
2021-11-08 11:34:46 +0100

    Add missing files

---
+++ src/main/java/glucobot/glucobotapi/GlucobotApiApplication.java
@@ -0,0 +1,13 @@
+package glucobot.glucobotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucobotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlucobotApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/glucobot/glucobotapi/GlucobotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package glucobot.glucobotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucobotApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
